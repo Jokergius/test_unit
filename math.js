7be32056411903a8c95a2dfe0443fce6277686eb
Jokergius <deniskudinov6926@gmail.com>
2025-04-07 18:26:43 +0200

    created multi, subtract, factorial functions

--- math.js
+++ math.js
@@ -4,7 +4,21 @@ function returnAddComponents (a, b) {
 }
 
 function returnDivideComponents(a, b){
+    if(b === 0) throw new Error("Division by zero");
     return a / b;
 }
 
-module.exports = { returnAddComponents, returnDivideComponents};
+function returnMultiplyComponents(a, b){
+    return a * b;
+}
+
+function returnSubtractComponents(a, b){
+    return a - b;
+}
+
+function returnFactorial(n){
+    if(n<0) throw new Error("Negative number");
+    return n<=1 ? 1 : n * returnFactorial (n - 1);
+}
+
+module.exports = { returnAddComponents, returnDivideComponents, returnMultiplyComponents, returnSubtractComponents, returnFactorial};
