6010f71db53ea0994a13f5547b5b45495bb2c9d1
Jokergius <deniskudinov6926@gmail.com>
2025-04-09 15:03:00 +0200

    Created tests for multiply, subtract and factroial functions

--- math.test.js
+++ math.test.js
@@ -6,3 +6,15 @@ test ('adds 2+3 to equal 5', () =>{
 test ('divide 10/5 to equal 2', () => {
     expect(returnDivideComponents(10, 5)).toBe(2);
 });
+
+test ('multiply 7*6 to equal 42', () => {
+    expect(returnMultiplyComponents(7, 6)).toBe(42);
+});
+
+test ('subtract 94 - 11 to equal 83', () => {
+    expect(returnSubtractComponents(94, 11)).toBe(83);
+});
+
+test ('computes factorial correctly', () => {
+    expect(returnFactorial(5)).toBe(120);
+});
